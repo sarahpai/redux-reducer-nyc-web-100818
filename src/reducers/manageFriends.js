f873e0dd5d2119c3e4ae328bbe2dd07028bef701
Sarah Pai <pai.sarah89@gmail.com>
2019-01-05 06:19:40 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+	switch (action.type) {
+		case "ADD_FRIEND":
+			return {  ...state, friends: [...state.friends, action.friend]  }
+		case 'REMOVE_FRIEND':
+			const filterFriends = state.friends.filter(friend => friend.id !== action.id);
+			return ({friends: filterFriends})
+		default:
+			return state;
+	}
 }
